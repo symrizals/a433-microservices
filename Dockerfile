d03b7f235b105099fd9b10250311e324979f1f2e
symrizals <sym_rizals@yahoo.com>
2026-02-24 22:17:48 +0000

    Add comments, restart policy, log.txt, and link.txt

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,20 @@
+# Menggunakan base image Node.js versi 14
 FROM node:14
+
+# Menentukan working directory di dalam container
 WORKDIR /app
+
+# Menyalin seluruh source code ke working directory
 COPY . .
+
+# Menentukan environment variable untuk production mode dan database host
 ENV NODE_ENV=production DB_HOST=item-db
+
+# Menginstal dependencies untuk production dan build aplikasi
 RUN npm install --production --unsafe-perm && npm run build
+
+# Mengekspos port yang digunakan aplikasi
 EXPOSE 8080
+
+# Menjalankan server saat container diluncurkan
 CMD ["npm", "start"]
--- build_push_image.sh
+++ build_push_image.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# 1. Build Docker image
+# Menyimpan password Docker Hub ke environment variable
+export PASSWORD_DOCKER_HUB=jr7fsPFcptcX3Me
+
+# 1. Membuat Docker image dari Dockerfile dengan nama item-app dan tag v1
 docker build -t item-app:v1 .
 
-# 2. Lihat daftar image lokal
+# 2. Melihat daftar image di lokal
 docker images
 
-# 3. Tag image sesuai format Docker Hub
+# 3. Mengubah nama image agar sesuai dengan format Docker Hub
 docker tag item-app:v1 symrizals/item-app:v1
 
-# 4. Login ke Docker Hub
-docker login
+# 4. Login ke Docker Hub menggunakan environment variable
+echo $PASSWORD_DOCKER_HUB | docker login -u symrizals --password-stdin
 
-# 5. Push image ke Docker Hub
+# 5. Mengunggah image ke Docker Hub
 docker push symrizals/item-app:v1
