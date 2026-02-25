3db4908acd81603278453428d93c1a0a00523ba8
symrizals <symrizals@gmail.com>
2026-02-25 21:28:57 +0000

    add build script

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Menyimpan password Docker Hub ke environment variable
+export PASSWORD_DOCKER_HUB=jr7fsPFcptcX3Me
+
+# 1. Membuat Docker image dari Dockerfile dengan nama karsajobs
+docker build -t karsajobs:latest .
+
+# 2. Melihat daftar image di lokal
+docker images
+
+# 3. Mengubah nama image agar sesuai dengan format Docker Hub
+docker tag karsajobs:latest symrizals/karsajobs:latest
+
+# 4. Login ke Docker Hub menggunakan environment variable
+echo $PASSWORD_DOCKER_HUB | docker login -u symrizals --password-stdin
+
+# 5. Mengunggah image ke Docker Hub
+docker push symrizals/karsajobs:latest
